9403ae59ad7d9ca732a8c24091ebc3c09be8bc38
SandeepHemba <sandeephemba@gmail.com>
2026-04-01 17:52:31 +0530

    Add method to send registration success email

--- src/main/java/com/hackathon/hacksmdc/service/EmailService.java
+++ src/main/java/com/hackathon/hacksmdc/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.hackathon.hacksmdc.service;
 
+import com.hackathon.hacksmdc.dto.RegistrationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -36,5 +37,54 @@ public class EmailService {
         message.setText(body);
 
         mailSender.send(message);
+    }
+
+    public void sendRegistrationSuccessEmail(String toEmail, String teamId, RegistrationRequest data) {
+
+        String subject = "🎉 Hackathon Registration Successful";
+
+        String body = """
+            Hello %s 👋,
+
+            🎉 Your team has been successfully registered!
+
+            🔖 Team ID: %s
+            👥 Team Name: %s
+            🏫 College: %s
+            💻 Domain: %s
+
+            📌 Team Members:
+            - %s
+            - %s
+            - %s
+
+            🚀 Keep this Team ID safe for future reference.
+
+            All the best for the hackathon! 💪
+            - HacksMDC Team
+            """.formatted(
+                data.getLeadName(),
+                teamId,
+                data.getTeamName(),
+                data.getCollege(),
+                data.getDomain(),
+                getMember(data, 0),
+                getMember(data, 1),
+                getMember(data, 2)
+        );
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+
+    private String getMember(RegistrationRequest data, int index) {
+        if (data.getMembers() != null && data.getMembers().size() > index) {
+            return data.getMembers().get(index);
+        }
+        return "-";
     }
 }
